4f13e593e70c927e7aedb20117941ba081b0a705
zoubjd <zouhairbajdouri19@gmail.com>
2024-04-05 12:28:35 +0000

    pop

--- morenodes.c
+++ morenodes.c
@@ -7,13 +7,14 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
+    stack_t *temp = *stack;
+    
     if (*stack == NULL)
     {
         fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    stack_t *temp = *stack;
     *stack = temp->next;
 
     if (*stack != NULL)
