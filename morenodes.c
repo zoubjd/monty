81539f5ecd8d2e4a1cbff234f0ec54546c11b1f7
zoubjd <zouhairbajdouri19@gmail.com>
2024-04-06 03:51:21 +0000

    swap

--- morenodes.c
+++ morenodes.c
@@ -37,7 +37,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
     {
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
         exit(EXIT_FAILURE);
     }
 	tmp = (*stack)->next;
