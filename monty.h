e0987a10426035df3c07e859f493ab4d6bdfa2d5
zoubjd <zouhairbajdouri19@gmail.com>
2024-04-06 06:19:08 +0000

    sub

--- monty.h
+++ monty.h
@@ -52,6 +52,7 @@ void pop(__attribute__((unused))stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 
 /*file*/
 void read_file(FILE *file);
